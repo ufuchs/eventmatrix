f16464985e65fbad442a5d3f181f07a3f969dd78
Uli Fuchs <ufuchs@gmx.com>
2011-07-22 18:11:58 +0200

    added : project 'freedays' the MANIFEST.MF

--- eventmatrix-examples/freedays/src/main/java/de/z35/commons/collections/exam/freedays/ShiftHolidayStrategy_en_US.java
+++ eventmatrix-examples/freedays/src/main/java/de/z35/commons/collections/exam/freedays/ShiftHolidayStrategy_en_US.java
@@ -16,10 +16,55 @@ import java.util.Calendar;
  */
 public class ShiftHolidayStrategy_en_US implements ShiftHolidayStrategy {
 
+	private static String FRIDAY = "Friday";
+	private static String MONDAY = "Monday";
+
+	/**
+	 * Creates an extra holiday closest to the next weekday.
+	 *
+	 * @param originData The <code>date</code> of the originally holiday.
+	 * @param originName The <code>name</code> of the originally holiday.
+	 * @param shiftUp If <code>true</code> shifts plus one day otherwise minus
+	 * 					one day.
+	 * @return An extra holiday on Friday or on Monday.
+	 */
+	private Holiday createShifted(Calendar originData, String originName,
+								  boolean shiftUp) {
+
+		String name = String.format("%s : Shifted to %s", originName,
+				((shiftUp) ? MONDAY : FRIDAY));
+
+		Calendar date = (Calendar) originData.clone();
+
+		date.roll(Calendar.DATE, shiftUp);
+
+		return Holiday.createHoliday(date, name);
+
+	}
+
+	/**
+	 * If a holiday falls on a weekend, an extra day close to the nearest
+	 * weekday will be created.
+	 *
+	 * @param origin The originally holiday
+	 * @return A new holiday if <code>origin</code> falls on a weekend otherwise
+	 * 			null
+	 */
 	@Override
 	public Holiday shiftHoliday(Holiday origin) {
-		
-		return null;
+
+		if (!origin.isOnWeekEnd()) {
+			return null;
+		}
+
+		Calendar date = origin.getDate();
+
+		String name = origin.getName();
+
+		boolean shiftUp = date.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY;
+
+		return createShifted(date, name, shiftUp);
+
 	}
 
 }
